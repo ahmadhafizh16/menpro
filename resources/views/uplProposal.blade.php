d7a59406f68a91e6e07855e9eb1d5238f4c9f007
Ahmad hafizh <ahmadhafizh96@gmail.com>
2020-06-26 09:15:48 +0700

    upd

--- resources/views/uplProposal.blade.php
+++ resources/views/uplProposal.blade.php
@@ -300,7 +300,7 @@ var app = new Vue({
 			formData.append('judulFile', this.judulFile);
 			formData.append('keterangan', this.keterangan);
 
-			axios.post( '/uploadProp',
+			axios.post( {{ url('/uploadProp') }},
 						formData,
 						{
 							headers: {
